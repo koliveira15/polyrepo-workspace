319af652fdae2c3b4293b3712c03ebbbfe530024
koliveira15 <koliveira15@gmail.com>
2023-01-18 00:06:51 -0500

    initial2

---
+++ workspace-one/apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'workspace-one-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
